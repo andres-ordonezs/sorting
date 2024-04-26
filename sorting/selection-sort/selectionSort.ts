c640b8b2fdbb8948d75df5022cd01611cf5bc877
andres-ordonezs <andresmw.89@gmail.com>
2024-04-26 14:56:07 -0400

    selection sort

--- sorting/selection-sort/selectionSort.ts
+++ sorting/selection-sort/selectionSort.ts
@@ -1,7 +1,21 @@
 /** Sort in-place and return sorted array. */
 
 function selectionSort(nums: number[]): number[] {
+  for (let i = 0; i < nums.length - 1; i++) {
+    let min = nums[i];
+
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] < min) {
+        min = nums[j];
+      }
+
+      if (nums[i] !== min) {
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+      }
+    }
+  }
+
   return nums;
 }
 
-export { selectionSort };
+export {selectionSort};
